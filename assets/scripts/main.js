29e145086095e3fa52874faf8c0d8e83362f4bf7
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2025-02-04 01:40:20 +0500

    Fix header margin behavior during scroll
    - Adjust header margins dynamically during scroll events
    - Restore initial margin values when returning to page top

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,16 @@
 window.addEventListener("scroll", () => {
   const header = document.querySelector(".header");
+  const navPrimary = document.querySelector("nav");
   if (window.scrollY > 1) {
     header.style.backgroundColor = "var(--secondary-clr)";
     header.style.boxShadow = "0 0.25rem 0.75rem var(--shadow-clr)";
+    navPrimary.classList.remove("nav-primary");
+    navPrimary.classList.add("nav-scrolled");
   } else {
     header.style.backgroundColor = "transparent";
     header.style.boxShadow = "none";
+    navPrimary.classList.remove("nav-scrolled");
+    navPrimary.classList.add("nav-primary");
   }
 });
 
